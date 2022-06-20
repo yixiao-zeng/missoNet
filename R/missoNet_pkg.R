d69450b527d3a4634d64f7a53a289f4fb8e92311
Yixiao Zeng <yixiao.zeng@qq.com>
2022-06-20 01:37:53 -0400

    exclude compiled binary files from git.

--- R/missoNet_pkg.R
+++ R/missoNet_pkg.R
@@ -1,4 +1,4 @@
-#' missoNet: a package for multivariate regression with missing outputs and conditional network estimation.
+#' missoNet: a package for multivariate regression and conditional network estimation with missing outputs.
 #'
 #' The missoNet package provides three categories of important functions:
 #' cv.missoNet, plot/predict and generateData.
@@ -7,7 +7,7 @@
 #' cv.missoNet: fit missoNet with the optimal tuning parameters selected by a k-fold cross-validation.
 #' plot: S3 method for plotting the cross-validation errors from a "cv.missoNet" object.
 #' predict: S3 method for making predictions from a "cv.missoNet" object.
-#' generateData: generate simulation data by a certain type of missing mechanism.
+#' generateData: generate simulation data by specifying a certain type of missing mechanism.
 #'
 #' @docType package
 #' @name missoNet
