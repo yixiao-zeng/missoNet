2f85b8f04c22d55df8489f1a3e83aca4e8047c72
Yixiao Zeng <yixiao.zeng@qq.com>
2022-06-21 02:05:14 -0400

    add missoNet-package documentation.

--- R/missoNet-package.R
+++ R/missoNet-package.R
@@ -1,16 +1,17 @@
 #' missoNet: a package for multivariate regression and conditional network estimation with missing outputs.
 #'
 #' The missoNet package provides three categories of important functions:
-#' cv.missoNet, plot/predict and generateData.
+#' cv.missoNet/missoNet, plot/predict and generateData.
 #' 
 #' @section missoNet functions:
-#' cv.missoNet: fit missoNet with the optimal tuning parameters selected by a k-fold cross-validation.
+#' cv.missoNet: fit missoNet with the optimal regularization parameters selected by a k-fold cross-validation.
+#' missoNet: fit missoNet with a given pair of regularization parameters.
 #' plot: S3 method for plotting the cross-validation errors from a "cv.missoNet" object.
 #' predict: S3 method for making predictions from a "cv.missoNet" object.
 #' generateData: generate simulation data by specifying a certain type of missing mechanism.
 #'
 #' @docType package
-#' @name missoNet
+#' @name missoNet-package
 #' @useDynLib missoNet, .registration=TRUE
 #' @importFrom Rcpp evalCpp
 NULL
